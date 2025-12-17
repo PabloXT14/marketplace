d64ee4f1b8b100166a27819d076fcda45baac3fa
Pablo Alan <pabloxt14@gmail.com>
2025-12-17 19:52:06 -0300

    chore: config lint

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,16 @@
-import { Text, View } from "react-native";
+import { Text, View } from "react-native"
 
 export default function Home() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 20 }}>Hello World</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#121214",
+      }}
+    >
+      <Text style={{ fontSize: 20, color: "#fff" }}>Home</Text>
     </View>
   )
 }

e91e9f500f0880db343daa93b0067049543d1b11
Pablo Alan <pabloxt14@gmail.com>
2025-12-18 20:15:17 -0300

    chore: config react query

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,11 +1,16 @@
 import { Stack } from "expo-router"
+import { QueryClientProvider } from "@tanstack/react-query"
 
 import "@/styles/global.css"
 
+import { queryClient } from "@/lib/query"
+
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{ headerShown: false, animation: "slide_from_right" }}
-    />
+    <QueryClientProvider client={queryClient}>
+      <Stack
+        screenOptions={{ headerShown: false, animation: "slide_from_right" }}
+      />
+    </QueryClientProvider>
   )
 }

62291a80517c9d57caac655f5740a6b0840328e9
Pablo Alan <pabloxt14@gmail.com>
2025-12-22 15:36:29 -0300

    feat: create use-register-mutation

---
+++ mobile/src/shared/queries/auth/use-register.mutation.ts
@@ -0,0 +1,19 @@
+import { useMutation } from "@tanstack/react-query"
+// biome-ignore lint/performance/noNamespaceImport: needed for authService
+import * as authService from "@/shared/services/auth.service"
+import type { RegisterHttpRequest } from "@/shared/interfaces/http/register"
+
+export const useRegisterMutation = () => {
+  const mutation = useMutation({
+    mutationFn: (userData: RegisterHttpRequest) =>
+      authService.registerService(userData),
+    onSuccess: (response) => {
+      console.log("Registration successful:", response)
+    },
+    onError: (error) => {
+      console.error("Registration failed:", error)
+    },
+  })
+
+  return mutation
+}

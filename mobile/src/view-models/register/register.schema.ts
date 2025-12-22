2a1fd95707174e5fa56c513b522ff6403adfe32d
Pablo Alan <pabloxt14@gmail.com>
2025-12-22 15:48:50 -0300

    feat: create register form schema

---
+++ mobile/src/view-models/register/register.schema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod"
+
+export const registerSchema = z
+  .object({
+    name: z.string().min(3, "Nome deve ter pelo menos 3 caracteres"),
+    email: z.email("Email inválido"),
+    password: z.string().min(6, "Senha deve ter pelo menos 6 caracteres"),
+    confirmPassword: z
+      .string()
+      .min(6, "Confirmação de senha deve ter pelo menos 6 caracteres"),
+    phone: z
+      .string()
+      .nonempty("Telefone é obrigatório")
+      .regex(
+        /^\d{11}$/,
+        "Telefone deve conter 11 dígitos numéricos (DDD + número)"
+      ),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    error: "As senhas não coincidem",
+    path: ["confirmPassword"],
+  })

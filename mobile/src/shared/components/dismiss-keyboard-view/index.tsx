d3c14f9c64d940c276695935d26bdf16795b3f0d
Pablo Alan <pabloxt14@gmail.com>
2026-01-05 19:01:21 -0300

    refactor: fix padding bottom error on android with keyboard-avoiding-view

--- mobile/src/shared/components/dismiss-keyboard-view/index.tsx
+++ mobile/src/shared/components/dismiss-keyboard-view/index.tsx
@@ -1,34 +1,22 @@
 import type { ReactNode } from "react"
-import {
-  Keyboard,
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
-  TouchableWithoutFeedback,
-} from "react-native"
+import { Keyboard, TouchableWithoutFeedback } from "react-native"
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
 
 type DismissKeyboardViewProps = {
   children: ReactNode
 }
 
-const IOS_KEYBOARD_VERTICAL_OFFSET = 64
-
 export const DismissKeyboardView = ({ children }: DismissKeyboardViewProps) => (
-  <KeyboardAvoidingView
-    style={{ flex: 1 }}
-    behavior={Platform.OS === "ios" ? "padding" : "height"}
-    keyboardVerticalOffset={
-      Platform.OS === "ios" ? IOS_KEYBOARD_VERTICAL_OFFSET : 0
-    }
-  >
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ flexGrow: 1 }}
-        keyboardShouldPersistTaps="handled"
-      >
-        {children}
-      </ScrollView>
-    </TouchableWithoutFeedback>
-  </KeyboardAvoidingView>
+  <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <KeyboardAwareScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ flexGrow: 1 }}
+      keyboardShouldPersistTaps="handled"
+      enableOnAndroid
+      enableAutomaticScroll
+      extraScrollHeight={20}
+    >
+      {children}
+    </KeyboardAwareScrollView>
+  </TouchableWithoutFeedback>
 )

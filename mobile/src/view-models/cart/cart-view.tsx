030ae4e0f45c7d46a369b34264ce0c9532c3e265
Pablo Alan <pabloxt14@gmail.com>
2026-02-23 18:34:16 -0300

    feat: start confirm budget section

--- mobile/src/view-models/cart/cart-view.tsx
+++ mobile/src/view-models/cart/cart-view.tsx
@@ -6,19 +6,21 @@ import type { useCartViewModel } from "./use-cart-view-model"
 import { ProductCard } from "./components/product-card"
 import { CartHeader } from "./components/cart-header"
 import { EmptyList } from "./components/empty-list"
+import { CartFooter } from "./components/cart-footer"
 
 type CartViewProps = ReturnType<typeof useCartViewModel>
 
 export const CartView = ({ products }: CartViewProps) => (
   <SafeAreaView edges={["top"]} className="flex-1 bg-background">
     <FlatList
-      data={[]}
+      data={products}
       keyExtractor={(item) => `cart-item-${item.id}`}
       renderItem={({ item }) => <ProductCard product={item} />}
       className="px-6 pt-9"
       contentContainerStyle={{ gap: 8, paddingBottom: 20 }}
       ListHeaderComponent={<CartHeader />}
       ListEmptyComponent={<EmptyList />}
+      ListFooterComponent={<CartFooter />}
     />
   </SafeAreaView>
 )
---
+++ mobile/src/view-models/cart/components/cart-footer/index.tsx
@@ -0,0 +1,49 @@
+import { Text, TouchableOpacity, View } from "react-native"
+
+import { colors } from "@/styles/colors"
+
+import { Button } from "@/shared/components/button"
+import { PriceText } from "@/shared/components/price-text"
+import { AppIcon } from "@/shared/components/app-icon"
+
+export const CartFooter = () => {
+  return (
+    <View className="mt-5 gap-4 rounded-lg bg-white p-4">
+      {/* TEXTS */}
+      <View className="gap-3 py-2">
+        {/* VALUE */}
+        <View className="flex-row items-center justify-between gap-3">
+          <Text className="font-lato-bold text-gray-400 text-xs uppercase leading-tight">
+            Valor total
+          </Text>
+
+          <PriceText
+            value={1426.29}
+            currencyClassName="text-base"
+            valueClassName="text-base"
+          />
+        </View>
+
+        {/* PAYMENT */}
+        <View className="flex-row items-center justify-between gap-3">
+          <Text className="font-lato-bold text-gray-400 text-xs uppercase leading-tight">
+            Pagamento
+          </Text>
+
+          <TouchableOpacity
+            className="flex-row items-center justify-center gap-2 p-0.5"
+            activeOpacity={0.7}
+          >
+            <AppIcon name="Card" size={20} color={colors.purple.base} />
+
+            <Text className="font-lato-bold text-purple-base text-sm leading-tight">
+              Adicionar cartão
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <Button text="Confirmar compra" />
+    </View>
+  )
+}

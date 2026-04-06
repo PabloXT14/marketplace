ae00735141cde0ed7282327b7a76f0af0cbdaab9
Pablo Alan <pabloxt14@gmail.com>
2026-04-06 17:23:19 -0300

    feat: config local notifications service

---
+++ mobile/src/shared/services/local-notifications-service.ts
@@ -0,0 +1,32 @@
+/** biome-ignore-all lint/performance/noNamespaceImport: needed */
+
+import { Platform } from "react-native"
+import * as Notifications from "expo-notifications"
+
+import { colors } from "@/styles/colors"
+
+const DEFAULT_CHANNEL = "default-channel"
+const NOTIFICATION_IDS = {
+  CART_REMINDER: "cart-reminder",
+  PURCHASE_FEEDBACK: "purchase-feedback",
+}
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+})
+
+export const setUpNotificationChannel = async () => {
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync(DEFAULT_CHANNEL, {
+      name: "Notificações do Marketplace",
+      importance: Notifications.AndroidImportance.HIGH,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: colors.purple.base,
+    })
+  }
+}
